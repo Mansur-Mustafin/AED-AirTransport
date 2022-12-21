d20358bc38508cf58abd9ba1f319e2749f3c4145
Ian <ianbeltrao@gmail.com>
2022-12-21 18:32:42 -0300

    Doxygen for flight.h

--- Code/Flight.h
+++ Code/Flight.h
@@ -21,16 +21,27 @@ using namespace std;
 
 class Flight {
 private:
+
+    /// @brief Flight origin
     string  source;
+
+    /// @brief Flight destination
     Target target;
 public:
 
+    /// @brief Empty constructor
     Flight() : target(Target()), source("") {}
 
+    /**
+     * @brief Constructor that receives input line from csv file
+     * @param in -> input line from csv file
+     */
     Flight(string in);
 
+    /// @brief Gets flight destination
     Target getTarget();
 
+    /// @brief gets flight origin
     string getSource();
 };
 
