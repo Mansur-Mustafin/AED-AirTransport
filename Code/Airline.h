5ff2a9667aa54f98cffa64ffc03d5be19d1f3cd9
Ian <ianbeltrao@gmail.com>
2022-12-21 18:21:43 -0300

    Doxygen for airline.h

--- Code/Airline.h
+++ Code/Airline.h
@@ -22,12 +22,29 @@ private:
     string code,name,callsign,country;
     unordered_set<string> targetsAirports;
 public:
+
+    /// @brief Empty Constructor
     Airline();
+
+    /**
+     * @brief Constructor that receives input line from csv file
+     * @param in -> input line from csv file
+     */
     explicit Airline(const string& in);
+
+    /// @brief Gets airline name
     string getName() const;
+
+    /// @brief Gets airline code
     string getCode() const;
+
+    /// @brief Gets airline call sign
     string getCallsign() const;
+
+    /// @brief Gets airline country
     string getCountry() const;
+
+    /// @brief Overload of <em>operator<< </em>
     friend ostream& operator<< (ostream& out, const Airline& s1);
 };
 
