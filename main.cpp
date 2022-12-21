f70b111c43ba7beaf94c46ba9956094929de6c25
Mansur <up202102355@fe.up.pt>
2022-12-21 19:12:46 +0000

    Bug

--- main.cpp
+++ main.cpp
@@ -9,17 +9,17 @@ int main() {
 
     Graph g;
 
-    //vector <string> ans = g.getUltimatePath("OPO", "Russia");
+    vector <string> ans = g.getUltimatePath("Brazil", "LIS");
     //vector <string> ans = g.getPathAirports("IOS", "HNL");
     //vector <string> ans = g.getPathCities("Kazan", "Moscow");
-    //vector <string> ans = g.getPathCountries("Kyrgyzstan","Kyrgyzstan");
+    //vector <string> ans = g.getPathCountries("Brazil","Portugal");
     //vector <string> ans = g.targetAirports("IOS", 1);
     //unordered_set<string> ans = g.getAirlinesFromAirport("IOS");
     //auto ans = g.getAirlines();
     //for(auto i : ans) cout << g.getAirlines()[i.first] << endl;
 
 
-    //for (int i = 0; i < ans.size(); i++) {  cout << ' ' << ans[i];  if (i != ans.size() - 1) cout << endl; }
+    for (int i = 0; i < ans.size(); i++) {  cout << ' ' << ans[i];  if (i != ans.size() - 1) cout << endl; }
 
 
     //cout << endl << g.getNumberOfFlights("IOS");
